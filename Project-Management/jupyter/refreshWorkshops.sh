a8648a0b1f20aeeda6c5b77d3d4e4d60b7442b0b
Karl Benedict <kbene@unm.edu>
2018-04-22 10:06:44 -0600

    add script comments

--- Project-Management/jupyter/refreshWorkshops.sh
+++ Project-Management/jupyter/refreshWorkshops.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash 
 
+# copy the utility notebook for calculating a new password hash into the top
+# level of the platform
 cd /home/jovyan
 cp /home/root/SHA1PasswordCalculator.ipynb .
 
+# remove any previously pulled copy of a workshop repository and perform a
+# fresh pull from GitHub
 rm -rf workshops-pm
 git clone https://github.com/karlbenedict/workshops-pm.git
-
+# change the initial location in the platform to the workshop folder
 cd /home/jovyan/workshops-pm
-cp /home/root/setGitOptions.sh .
 cp /home/root/setGitOptions.sh .
