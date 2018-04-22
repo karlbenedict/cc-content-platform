77c85605707caff6330d3caf553ba6625d66048c
Karl Benedict <kbene@unm.edu>
2018-04-22 08:35:52 -0600

    integrated password calculator into file setup

--- Project-Management/jupyter/refreshWorkshops.sh
+++ Project-Management/jupyter/refreshWorkshops.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash 
 
 cd /home/jovyan
+cp /home/root/SHA1PasswordCalculator.ipynb .
 
 rm -rf workshops-pm
 git clone https://github.com/karlbenedict/workshops-pm.git
 
 cd /home/jovyan/workshops-pm
+cp /home/root/setGitOptions.sh .
 cp /home/root/setGitOptions.sh .
